5929e2dc4834b91170b3497d536d0581a8265da6
Scott Harvey <scottyharvey33@gmil.com>
2016-07-14 17:36:43 -0600

    All angular and ui-routers are linked with backend, also fixed gulp to see all images and all html

--- config.js
+++ config.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 module.exports = {
   'secret': 'd0nttellthefr0ntend',
     'database': 'mongodb://localhost/9000'
-=======
-mondule exports {
-  "secret": "d0nttellthefr0ntend"
->>>>>>> master
 }
