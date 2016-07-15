17d7d791dfb504e1485e0323fe0499f9c5a19029
Chris Myers <7chrismmyers7@gmail.com>
2016-07-15 16:54:43 -0600

    create Local Auth

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
   name: {type: String},
@@ -6,7 +7,26 @@ var userSchema = new mongoose.Schema({
   password: {type: String},
   identifier: {type: String},
   setLocation: [{type: Object}],
-  vehicles [{type: String, ref: 'Vehicle'}]
+  vehicles: [{type: String, ref: 'Vehicle'}]
 })
 
-module.export = mongoose.model('User', userSchema);
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = (password, hash, done) => {
+  bcrypt.compare(password, hash, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
--- serverControllers/userCtrl.js
+++ serverControllers/userCtrl.js
@@ -1,14 +1,8 @@
-const Admin = require('./models/admin.js');
-const User = require('./models/user.js');
-const Vehicle = require('./models/vehicle.js');
+const Admin = require('../models/admin.js');
+const User = require('../models/user.js');
+const Vehicle = require('../models/vehicle.js');
 
 module.exports = {
-
-
-
-
-
-
 
 
 }
