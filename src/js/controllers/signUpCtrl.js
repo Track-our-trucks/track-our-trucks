d2bdf73e7963955681db46d554c839a77efa76d7
Scott Gourley <scottgourley@Scotts-MacBook-Air.local>
2016-07-19 11:29:23 -0600

    add frontend logic for localAuth for user signup/login and admin sighup/login

--- src/js/controllers/signUpCtrl.js
+++ src/js/controllers/signUpCtrl.js
@@ -1,8 +1,8 @@
-angular.module('trackOurTruck').controller('signUpCtrl', function($scope, $state, userService) {
+angular.module('trackOurTruck').controller('signUpCtrl', function($auth, $scope, $state, userService) {
 
 $scope.signUp = function(user){
   userService.signUp(user).then(function(response){
-    
+    $auth.setToken(response);
   })
 }
 
