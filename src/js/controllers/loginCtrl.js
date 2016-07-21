8143bff890e74cf5a00ce9d1a712c7a011846ceb
Chris Myers <7chrismmyers7@gmail.com>
2016-07-21 16:46:02 -0600

    add functionality to frontend to dynamically get user info

--- src/js/controllers/loginCtrl.js
+++ src/js/controllers/loginCtrl.js
@@ -5,6 +5,7 @@ angular.module('trackOurTruck').controller('loginCtrl', ($auth, $scope, $state, 
       $auth.setToken(response);
       console.log(response);
       if (response.status === 200) {
+        userService.currentUser = response.data.user;
         $state.go('userHome')
       }
     })
