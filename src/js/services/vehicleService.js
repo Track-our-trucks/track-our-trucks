5e834edd5bd5c3ba367f055a34f8d42af1e16c32
Scott Harvey <scottyharvey33@gmil.com>
2016-07-21 18:34:02 -0600

    Made data in controller scoping with ng-repeat and markers showing

--- src/js/services/vehicleService.js
+++ src/js/services/vehicleService.js
@@ -10,4 +10,11 @@ angular.module('trackOurTruck').service('vehicleService', function($http, $q){
     })
   }
 
+
+  this.getAddress = (pos) => {
+    return $http({
+      method: 'GET',
+      url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+pos.pos[0]+','+pos.pos[1]+'&key=AIzaSyCE-IsoxmgBiFRKXn0ZgZcYlLZ1FSvpJns'
+    })
+  }
 })
