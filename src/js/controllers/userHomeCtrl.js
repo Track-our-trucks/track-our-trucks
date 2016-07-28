f0275098062f6a8c5894c7e183f77400690d3314
Bob Cook <owner-mac@Bobs-MacBook-Pro.local>
2016-07-28 16:25:41 -0600

    fixed vehicle selection and markers

--- src/js/controllers/userHomeCtrl.js
+++ src/js/controllers/userHomeCtrl.js
@@ -37,6 +37,19 @@ angular.module('trackOurTruck').controller('userHomeCtrl', ($auth, $scope, $stat
   }
   })
 
+  //fake data
+
+  //
+  // var fakeData = [
+  //
+  //
+  //   {"fixTime":1469734913000,"lat":40.2271,"long":-111.659,"speed":18,"heading":234,"event":9,"address":"2-48 W 500 S, Provo, UT 84601, USA"},
+  //   {"fixTime":1469734928000,"lat":40.2269,"long":-111.6606,"speed":13,"heading":193,"event":9,"address":"522 S 100 W St, Provo, UT 84601, USA"},
+  //   {"fixTime":1469734949000,"lat":40.2258,"long":-111.6607,"speed":11,"heading":260,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  //   {"fixTime":1469734948000,"lat":40.2258,"long":-111.6606,"speed":8,"heading":219,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  //   {"fixTime":1469734952000,"lat":40.2258,"long":-111.6609,"speed":8,"heading":301,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  //   {"fixTime":1469734968000,"lat":40.2259,"long":-111.6609,"speed":0,"heading":315,"event":12,"address":"108 600 S, Provo, UT 84601, USA"}
+  // ]
 
   //getting the list of vehicles to show on the userHome page
 
@@ -44,12 +57,28 @@ angular.module('trackOurTruck').controller('userHomeCtrl', ($auth, $scope, $stat
     var payloadData = $auth.getPayload()
     userService.getUser(payloadData.sub).then(response => {
        $scope.vehicleArr = response.data.vehicles;
-       console.log($scope.vehicleArr);
+
+       $scope.getCurrentLocations()
+
     })
   }
   $scope.getVehicleList();
 
 
+  $scope.getCurrentLocations = () => {
+
+    var tmpArr = [];
+
+    for (var i = 0; i < $scope.vehicleArr.length; i++) {
+
+      tmpArr.push({pos:[$scope.vehicleArr[i].timeDistanceProfiles.lat,$scope.vehicleArr[i].timeDistanceProfiles.lat]})
+
+
+    }
+
+    $scope.currentLocations = tmpArr;
+
+  }
 
   $scope.modalOff = () => {
 
@@ -62,7 +91,9 @@ angular.module('trackOurTruck').controller('userHomeCtrl', ($auth, $scope, $stat
 
   }
 
-  $scope.vehicleModal = () => {
+  $scope.vehicleModal = selectedVehicle => {
+
+    vehicleService.selectedVehicle = selectedVehicle;
 
     $scope.getUser();
 
