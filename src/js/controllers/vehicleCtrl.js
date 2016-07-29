833667e905280c05582ed652f00c2563dc762337
Chris Myers <7chrismmyers7@gmail.com>
2016-07-28 22:12:59 -0600

    add filter in vehicleCtrl for time

--- src/js/controllers/vehicleCtrl.js
+++ src/js/controllers/vehicleCtrl.js
@@ -3,16 +3,16 @@ angular.module('trackOurTruck').controller('vehicleCtrl', ($scope, $auth, $state
 
 //FAKE DATE
 
-// var fakeData = [
-//
-//
-//   {"fixTime":1469734913000,"lat":40.2271,"long":-111.659,"speed":18,"heading":234,"event":9,"address":"2-48 W 500 S, Provo, UT 84601, USA"},
-//   {"fixTime":1469734928000,"lat":40.2269,"long":-111.6606,"speed":13,"heading":193,"event":9,"address":"522 S 100 W St, Provo, UT 84601, USA"},
-//   {"fixTime":1469734949000,"lat":40.2258,"long":-111.6607,"speed":11,"heading":260,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
-//   {"fixTime":1469734948000,"lat":40.2258,"long":-111.6606,"speed":8,"heading":219,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
-//   {"fixTime":1469734952000,"lat":40.2258,"long":-111.6609,"speed":8,"heading":301,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
-//   {"fixTime":1469734968000,"lat":40.2259,"long":-111.6609,"speed":0,"heading":315,"event":12,"address":"108 600 S, Provo, UT 84601, USA"}
-// ]
+var fakeData = [
+
+
+  {"fixTime":1469734913000,"lat":40.2271,"long":-111.659,"speed":18,"heading":234,"event":9,"address":"2-48 W 500 S, Provo, UT 84601, USA"},
+  {"fixTime":1469734928000,"lat":40.2269,"long":-111.6606,"speed":13,"heading":193,"event":9,"address":"522 S 100 W St, Provo, UT 84601, USA"},
+  {"fixTime":1469734949000,"lat":40.2258,"long":-111.6607,"speed":11,"heading":260,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  {"fixTime":1469734948000,"lat":40.2258,"long":-111.6606,"speed":8,"heading":219,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  {"fixTime":1469734952000,"lat":40.2258,"long":-111.6609,"speed":8,"heading":301,"event":9,"address":"108 600 S, Provo, UT 84601, USA"},
+  {"fixTime":1469734968000,"lat":40.2259,"long":-111.6609,"speed":0,"heading":315,"event":12,"address":"108 600 S, Provo, UT 84601, USA"}
+]
 
 
 
@@ -21,6 +21,8 @@ angular.module('trackOurTruck').controller('vehicleCtrl', ($scope, $auth, $state
 
 
 
+  $scope.showHidden = false;
+  $scope.resetHidden = true;
 
 
   $scope.tab = 1;
@@ -84,14 +86,22 @@ angular.module('trackOurTruck').controller('vehicleCtrl', ($scope, $auth, $state
 
             $scope.directions.push(newCompass);
 
+            if (!$scope.timeTo && !$scope.timeFrom && theDayPins[i].event === 12) {
               var addressWithTime = {
                   address: theDayPins[i].address,
                   time: new Date(theDayPins[i].fixTime)
+                }
+              }
+            else {
+              var addressWithTime = {
+                  address: theDayPins[i].address,
+                  time: new Date(theDayPins[i].fixTime)
+                }
               }
 
               newPos.push(addressWithTime);
+            }
 
-        }
               $scope.addresses = newPos;
 
               $scope.pins = newPin;
@@ -105,24 +115,46 @@ angular.module('trackOurTruck').controller('vehicleCtrl', ($scope, $auth, $state
 
     var filter = tracker => {
 
-      var theFilterer = val => {
+      var dateFilter = val => {
         var test1 = (new Date(val.fixTime)).toDateString();
         var test2 = (new Date($scope.theDate)).toDateString();
        return  test1 === test2;
       }
 
-    let theDayPins = tracker.filter(theFilterer);
+      var timeFilter = val => {
+          return (new Date(val.fixTime)).getHours() + (new Date(val.fixTime)).getMinutes() >= (new Date($scope.timeFrom)).getHours() + (new Date($scope.timeFrom)).getMinutes() && (new Date(val.fixTime)).getHours() + (new Date(val.fixTime)).getMinutes() <= (new Date($scope.timeTo)).getHours() +  (new Date($scope.timeTo)).getMinutes()
+      }
 
+    let dateFilteredArray = tracker.filter(dateFilter);
 
+    if ($scope.timeFrom && $scope.timeTo) {
+      $scope.showHidden = true;
+      $scope.resetHidden = false;
+      vehicleStopTimer();
+      var theDayPins = dateFilteredArray.filter(timeFilter)
+    }
+    else {
+      vehicleStopTimer();
+      vehicleTimer();
+      var theDayPins = dateFilteredArray;
+    }
 
 
      positionFilter(theDayPins);
 
     }
 
+    $scope.resetTime = () => {
+      $scope.showHidden = false;
+      $scope.resetHidden = true;
+      $scope.timeTo = '';
+      $scope.timeFrom = '';
+      $scope.getUserVehicle();
+    }
+
     $scope.selectedVehicle = vehicleService.selectedVehicle;
 
-    // var testcounter = 1; //FAKE DATA
+    var testcounter = 1; //FAKE DATA
     $scope.getUserVehicle = () => {
 
       if((new Date($scope.theDate)).toDateString() !== (new Date()).toDateString()){
