200a8d18bd20b913122d64ddb715a70f61d5e9ac
Bob Cook <owner-mac@Bobs-MacBook-Pro.local>
2016-07-30 03:17:34 -0600

    added Vehicle names to user home

--- src/js/controllers/userHomeCtrl.js
+++ src/js/controllers/userHomeCtrl.js
@@ -124,4 +124,22 @@ angular.module('trackOurTruck').controller('userHomeCtrl', ($auth, $scope, $stat
       $auth.logout();
       $state.go("welcome");
     }
+
+    $scope.showName = (event, index, location) => {
+
+       $scope.choiceOn = true;
+
+       $scope.choiceName = $scope.vehicleArr[index].name;
+       $scope.choiceCenter = location;
+
+
+
+
+
+    }
+
+    $scope.choiceOff= () => {
+
+      $scope.choiceOn = false;
+    }
 })
