deaddaec7032ad162c2312ac7004d1bf67912471
Scott Gourley <scottgourley@Scotts-MacBook-Air.local>
2016-07-29 22:05:41 -0600

    start behavior page

--- src/js/controllers/vehicleInfoCtrl.js
+++ src/js/controllers/vehicleInfoCtrl.js
@@ -16,6 +16,11 @@ angular.module('trackOurTruck').controller('vehicleInfoCtrl', function($scope, $
      return $scope.tab === tabNum;
    };
 
+   $scope.goToLocation = function() {
+     $state.go('userHome.vehicleInfo.location')
+   }
 
-
+   $scope.goToBehaviors = function() {
+     $state.go('userHome.behaviors')
+   }
 })
