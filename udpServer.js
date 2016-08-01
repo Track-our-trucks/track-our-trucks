9f6e591400246a1b87618cffbc5b8bcfe4acf9a2
Scott Gourley <scottgourley@Scotts-Air.PK5001Z>
2016-07-31 21:44:46 -0600

    fixed udp server error

--- udpServer.js
+++ udpServer.js
@@ -54,8 +54,8 @@ udpServer.on('message', (message, remote) => {
 
     axios.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+decoded.lat +','+ decoded.long +'&key=AIzaSyAhLCAAyP4IVHjWK3kf6Ts_kGII2jtX5qI').then(response => {
       decoded.address = response.data.results[0].formatted_address;
-      axios.get('https://roads.googleapis.com/v1/speedLimits?path=' + decoded.lat +','+ decoded.long +'&key=AIzaSyAhLCAAyP4IVHjWK3kf6Ts_kGII2jtX5qI').then(response => {
-        decoded.speedLimit = response.data;
+      // axios.get('https://roads.googleapis.com/v1/speedLimits?path=' + decoded.lat +','+ decoded.long +'&key=AIzaSyAhLCAAyP4IVHjWK3kf6Ts_kGII2jtX5qI').then(response => {
+      //   decoded.speedLimit = response.data;
 
         fs.appendFile(`./public/decodedLogFile.txt`, `${JSON.stringify(decoded)}\n`, err => {
             if (err) console.log(`ERROR writing to decoded logfile: ${err}`);
@@ -65,7 +65,7 @@ udpServer.on('message', (message, remote) => {
           if (err) console.log(`MONGO ERROR: ${err}`);
           else console.log(`MONGO SUCCESS: ${success}`);
         });
-      })
+      // })
 
 
     })
