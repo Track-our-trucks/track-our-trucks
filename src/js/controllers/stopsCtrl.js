30cd96da1d9842c5333fbca511d58e1b32b899e3
Scott Gourley <scottgourley9@gmail.com>
2016-08-02 11:09:51 -0600

    add stops date picker

--- src/js/controllers/stopsCtrl.js
+++ src/js/controllers/stopsCtrl.js
@@ -1,5 +1,6 @@
 angular.module('trackOurTruck').controller('stopsCtrl', function($state, $scope, vehicleService){
 
+  $scope.theDate = (new Date()).toDateString()
   $scope.vehicle = vehicleService.theSelectedVehicle
 
   $scope.back = () => {
@@ -7,16 +8,28 @@ angular.module('trackOurTruck').controller('stopsCtrl', function($state, $scope,
     $scope.wideModalOn = true;
   }
 
+
+
+
+  var dateFilter = val => {
+      var test1 = (new Date(val.fixTime)).toDateString();
+      var test2 = (new Date($scope.theDate)).toDateString();
+      return test1 === test2;
+  }
+
+  $scope.filteredByDate = $scope.vehicle.timeDistanceProfiles.filter(dateFilter)
+
+
   $scope.getStops = () => {
     var stopsArr = [];
-    for(var i = 0; i < $scope.vehicle.timeDistanceProfiles.length; i++){
-      if($scope.vehicle.timeDistanceProfiles[i].event === 12){
-        stopsArr.push($scope.vehicle.timeDistanceProfiles[i]);
+    for(var i = 0; i < $scope.filteredByDate.length; i++){
+      if($scope.filteredByDate[i].event === 12){
+        stopsArr.push($scope.filteredByDate[i]);
       }
     }
+    $scope.theStopsArray = stopsArr;
   }
   $scope.getStops();
-
 
 
 })
