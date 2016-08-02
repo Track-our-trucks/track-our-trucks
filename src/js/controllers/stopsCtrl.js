d7e894790f23ea39157d02971805f8963aaa2c05
Scott Gourley <scottgourley9@gmail.com>
2016-08-02 10:35:16 -0600

    add stops

--- src/js/controllers/stopsCtrl.js
+++ src/js/controllers/stopsCtrl.js
@@ -1,6 +1,22 @@
 angular.module('trackOurTruck').controller('stopsCtrl', function($state, $scope, vehicleService){
+
+  $scope.vehicle = vehicleService.theSelectedVehicle
+
   $scope.back = () => {
     $state.go('userHome.vehicleInfo.behaviors');
     $scope.wideModalOn = true;
   }
+
+  $scope.getStops = () => {
+    var stopsArr = [];
+    for(var i = 0; i < $scope.vehicle.timeDistanceProfiles.length; i++){
+      if($scope.vehicle.timeDistanceProfiles[i].event === 12){
+        stopsArr.push($scope.vehicle.timeDistanceProfiles[i]);
+      }
+    }
+  }
+  $scope.getStops();
+
+
+
 })
