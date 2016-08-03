6abb6f5395c3897219ee3e3aa8155417ff68e87f
Scott Gourley <scottgourley9@gmail.com>
2016-08-02 19:18:25 -0600

    l

--- src/js/controllers/vehicleCtrl.js
+++ src/js/controllers/vehicleCtrl.js
@@ -224,8 +224,8 @@ angular.module('trackOurTruck').controller('vehicleCtrl', ($scope, $auth, $state
     $scope.showDesc = (event, index, pin, dayLocations) => {
 
        $scope.choiceOn = true;
-
        $scope.choiceInfo = dayLocations[index];
+       $scope.theTime = new Date(dayLocations[index].fixTime)
        $scope.choiceCenter = pin;
 
       var compass = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
