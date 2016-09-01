0250384ea70f762b4b8a2a1c231207f64abf452e
Scott Gourley <scottgourley9@gmail.com>
2016-08-31 20:48:47 -0600

    add stoptime

--- src/js/controllers/stopsCtrl.js
+++ src/js/controllers/stopsCtrl.js
@@ -17,15 +17,15 @@ angular.module('trackOurTruck').controller('stopsCtrl', function($state, $scope,
         stopsArr.push(val[i]);
       }
     }
-    // for(let i = 0; i < stopsArr.length; i++){
-    //   var hours = Math.floor(stopsArr[i].stopTime/1000/60/60)
-    //   var minutes = Math.floor((stopsArr[i].stopTime/1000/60) - (hours * 60))
-    //   stopsArr[i].stopTime = hours + ':' + minutes;
-    // }
+    for(let i = 0; i < stopsArr.length; i++){
+      var hours = Math.floor(stopsArr[i].stopTime/1000/60/60)
+      var minutes = Math.floor((stopsArr[i].stopTime/1000/60) - (hours * 60))
+      stopsArr[i].stopTime = hours + ':' + minutes;
+    }
     $scope.stops = stopsArr;
 }
 
-  $scope.dateFilter = (val) => {
+  $scope.dateFilter = () => {
     var filteredByDate = [];
     for(var i = 0; i < $scope.vehicle.timeDistanceProfiles.length; i++){
       if((new Date($scope.vehicle.timeDistanceProfiles[i].fixTime)).toDateString() === (new Date($scope.theDate)).toDateString()){
@@ -36,29 +36,29 @@ angular.module('trackOurTruck').controller('stopsCtrl', function($state, $scope,
   }
 
 
-//   $scope.getStopTime = () => {
-//     $scope.theVehicleArr = []
-//     for(var i = 0; i < $scope.vehicle.timeDistanceProfiles.length; i++){
-//       if($scope.vehicle.timeDistanceProfiles[i].event === 12 && !$scope.vehicle.timeDistanceProfiles[i + 1]){
-//         dateFilter($scope.theVehicleArr)
-//         // $scope.stillThere = true;
-//         // $scope.showTime = false;
-//         return;
-//       }
-//       if($scope.vehicle.timeDistanceProfiles[i].event === 12 && $scope.vehicle.timeDistanceProfiles[i + 1].event === 11){
-//
-//         diff = Math.abs($scope.vehicle.timeDistanceProfiles[i + 1].fixTime - $scope.vehicle.timeDistanceProfiles[i].fixTime)
-//         $scope.vehicle.timeDistanceProfiles[i].stopTime = diff;
-//         $scope.theVehicleArr.push($scope.vehicle.timeDistanceProfiles[i])
-//         // $scope.stillThere = false;
-//         // $scope.showTime = true;
-//       }
-//       else if($scope.vehicle.timeDistanceProfiles[i].event === 12){
-//         $scope.theVehicleArr.push($scope.vehicle.timeDistanceProfiles[i])
-//       }
-//   }
-//   dateFilter(theVehicleArr);
-// }
+  $scope.getStopTime = () => {
+    $scope.theVehicleArr = []
+    for(var i = 0; i < $scope.vehicle.timeDistanceProfiles.length; i++){
+      if($scope.vehicle.timeDistanceProfiles[i].event === 12 && !$scope.vehicle.timeDistanceProfiles[i + 1]){
+        $scope.dateFilter($scope.theVehicleArr)
+        $scope.stillThere = true;
+        $scope.showTime = false;
+        return;
+      }
+      if($scope.vehicle.timeDistanceProfiles[i].event === 12 && $scope.vehicle.timeDistanceProfiles[i + 1].event === 11){
+
+        diff = Math.abs($scope.vehicle.timeDistanceProfiles[i + 1].fixTime - $scope.vehicle.timeDistanceProfiles[i].fixTime)
+        $scope.vehicle.timeDistanceProfiles[i].stopTime = diff;
+        $scope.theVehicleArr.push($scope.vehicle.timeDistanceProfiles[i])
+        $scope.stillThere = false;
+        $scope.showTime = true;
+      }
+      else if($scope.vehicle.timeDistanceProfiles[i].event === 12){
+        $scope.theVehicleArr.push($scope.vehicle.timeDistanceProfiles[i])
+      }
+  }
+  $scope.dateFilter(theVehicleArr);
+}
 
 
 
